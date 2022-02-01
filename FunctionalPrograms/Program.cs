5c2d579f2f1d7e675940a85cecc3a5650edc0c05
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 11:50:26 +0530

    [Rakshit] Add: Sum of 3 integer equals zero program completed

--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -17,7 +17,7 @@ namespace FunctionalPrograms
                 flag = true;
                 while (flag)
                 {
-                    Console.WriteLine("Enter 1: To enter for two dimensional array\n" + "Enter 2: To Exit\n");
+                    Console.WriteLine("Enter 1: To enter for two dimensional array\n" +  "Enter 2: To get sum of 3 added integer as zero\n" + "Enter 3: To Exit\n");
                     int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
@@ -26,6 +26,10 @@ namespace FunctionalPrograms
                             twoDimensionalArray.Matrix();
                             break;
                         case 2:
+                            SumAddsToZero sumAddsToZero = new SumAddsToZero();
+                            sumAddsToZero.SumofThreeZero();
+                            break;
+                        case 3:
                             flag = false;
                             break;
                         default:
